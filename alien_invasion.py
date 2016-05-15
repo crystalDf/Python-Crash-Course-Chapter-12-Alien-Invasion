47bc330029538642f237a60af95d7e49906e743d
crystalDf <chendong333@gmail.com>
2016-05-15 20:38:07 +0800

    Chapter 12 Alien Invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,9 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
     # Start the main loop for the game.
     while True:
 
@@ -16,6 +19,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
