f6f5822a90a7d9f49602a22a71cc51efff2ee005
crystalDf <chendong333@gmail.com>
2016-05-18 21:10:56 +0800

    Chapter 12 Alien Invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,10 @@
-import sys
+# import sys
 
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -24,18 +25,9 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
-
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
---
+++ game_functions.py
@@ -0,0 +1,20 @@
+import sys
+
+import pygame
+
+
+def check_events():
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
