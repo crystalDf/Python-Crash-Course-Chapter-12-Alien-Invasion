ff7185cd2fd9f1d184f69801dd76bfe1d0d8f8a0
crystalDf <chendong333@gmail.com>
2016-05-19 21:35:58 +0800

    Chapter 12 Alien Invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,8 +26,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        gf.check_events()
-
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -3,11 +3,16 @@ import sys
 import pygame
 
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # Move the ship to the right.
+                ship.rect.centerx += 1
+
 
 
 def update_screen(ai_settings, screen, ship):
