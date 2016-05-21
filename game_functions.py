ba710d647563f51d1bec22bcd54dbcef564b8c1e
crystalDf <chendong333@gmail.com>
2016-05-21 19:57:09 +0800

    Chapter 12 Alien Invasion

--- game_functions.py
+++ game_functions.py
@@ -11,9 +11,13 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
--- ship.py
+++ ship.py
@@ -15,13 +15,16 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        # Movement flag
+        # Movement flags
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
+        """Update the ship's position based on the movement flags."""
         if self.moving_right:
             self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
