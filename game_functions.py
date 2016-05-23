db7e341782b1755148cd91026a673ebf908826a9
crystalDf <chendong333@gmail.com>
2016-05-23 22:28:29 +0800

    Chapter 12 Alien Invasion

--- game_functions.py
+++ game_functions.py
@@ -9,15 +9,25 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+
+
+def check_keydown_events(event, ship):
+    """Respond to key presses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
--- ship.py
+++ ship.py
@@ -26,9 +26,9 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flags."""
         # Update the ship's center value, not the rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
         # Update rect object from self.center.
