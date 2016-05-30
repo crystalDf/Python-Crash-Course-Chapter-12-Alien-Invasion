66b0905d64941cc9381f9278ed248645258097db
crystalDf <chendong333@gmail.com>
2016-05-30 22:51:29 +0800

    Chapter 12 Alien Invasion

--- game_functions.py
+++ game_functions.py
@@ -77,14 +77,24 @@ def create_fleet(ai_settings, screen, aliens):
     # Create an alien and find the number of aliens in a row.
     # Spacing between each alien is equal to one alien width.
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
 
     # Create the first row of aliens.
     for alien_number in range(number_aliens_x):
-        # Create an alien and place it in the row.
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(ai_settings, screen, aliens, alien_number)
+
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    # Create an alien and place it in the row.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
+
+def get_number_aliens_x(ai_settings, alien_width):
+    """Determine the number of aliens that fit in a row."""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
