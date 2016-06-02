ffb05964500dbc56f7739a2d84433d95cd5b5c0d
crystalDf <chendong333@gmail.com>
2016-06-03 00:10:52 +0800

    Chapter 12 Alien Invasion

--- game_functions.py
+++ game_functions.py
@@ -60,7 +60,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
     # Update bullet positions.
     bullets.update()
@@ -70,6 +70,10 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
             # print(len(bullets))
+
+    # Check for any bullets that have hit aliens.
+    # If so, get rid of the bullet and the alien.
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 
 def create_fleet(ai_settings, screen, ship, aliens):
