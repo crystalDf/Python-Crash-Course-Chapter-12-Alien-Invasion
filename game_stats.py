902f85bef6fb10836d84b92d680110195912060b
crystalDf <chendong333@gmail.com>
2016-06-15 23:18:20 +0800

    Chapter 12 Alien Invasion

--- game_stats.py
+++ game_stats.py
@@ -12,3 +12,4 @@ class GameStats:
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
---
+++ scoreboard.py
@@ -0,0 +1,34 @@
+import pygame.ftfont
+
+
+class Scoreboard:
+    """A class to report scoring information."""
+
+    def __init__(self, ai_settings, screen, stats):
+        """Initialize scorekeeping attributes."""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
+        self.stats = stats
+
+        # Font settings for scoring information.
+        self.text_color = (30, 30, 30)
+        self.font = pygame.ftfont.SysFont(None, 48)
+
+        # Prepare the initial score image.
+        self.prep_score()
+
+    def prep_score(self):
+        """Turn the score into a rendered image."""
+        score_str = str(self.stats.score)
+        self.score_image = self.font.render(score_str, True, self.text_color,
+                                            self.ai_settings.bg_color)
+
+        # Display the score at the top right of the screen.
+        self.score_rect = self.score_image.get_rect()
+        self.score_rect.right = self.screen_rect.right - 20
+        self.score_rect.top = 20
+
+    def show_score(self):
+        """Draw score to the screen."""
+        self.score.blit(self.score_image, self.score_rect)
